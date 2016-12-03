a9a3dfd2a7ff0b4b5b827312a9e38caf393c1f8d
Philip Dai <smartielion@gmail.com>
2016-12-02 23:36:17 -0500

    Fix server.js typo

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(function (req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
-    res.redirect('http://' ++ req.hostname + req.url);
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
 

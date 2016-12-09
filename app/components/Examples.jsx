c92091906e208ef46f04452426d057ff7a744c02
Philip Dai <smartielion@gmail.com>
2016-12-08 22:07:23 -0500

    Adding styles in samples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,18 @@
 let React = require('react'),
+    {Link} = require('react-router'),
     Examples = () => {
       return (
         <div>
-          <h3>Examples</h3>
-          <p>Welcome to examples page!</p>
+          <h1 className="text-center">Examples</h1>
+          <p>Here are some example locations to try out:</p>
+          <ol>
+            <li>
+              <Link to="/?location=Columbus">Columbus, OH</Link>
+            </li>
+            <li>
+              <Link to="/?location=Rio">Rio, Brazil</Link>
+            </li>
+          </ol>
         </div>
       );
     };
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,10 +4,13 @@ let React = require('react'),
       return (
         <div>
           <Nav></Nav>
-          <h2>Main component</h2>
-          {props.children}
+          <div className="row">
+            <div className="columns medium-6 large-4 small-centered">
+              {props.children}
+            </div>
+          </div>
         </div>
       );
     };
-    
+
 module.exports = Main;
